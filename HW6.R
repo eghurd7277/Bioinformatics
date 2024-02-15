236bf9d3f4e9e9f4789fe88ff317fa3825d4b20a
eghurd7277 <eghurd7277@eagle.fgcu.edu>
2024-02-15 02:04:25 -0500

    Update HW6.R

--- HW6.R
+++ HW6.R
@@ -1,2 +1,13 @@
 writeXStringSet(rnaSeq1, "rufusAA.fasta", format="fasta")
 ProtSeq <- read.csv("protein_seq.fasta")
+AscNum <- ProtSeq[,1]
+GO_Info <- GetProteinGOInfo(AscNum)
+PlotGoInfo(GO_Info)
+PlotGOAll(GOObj = GO_Info, Top = 10, directorypath = getwd(), width = 8, height = 5)
+PathInfo <- GetPathology_Biotech(AscNum)
+Get.diseases(PathInfo, directorypath = getwd())
+UnProtData <- fetch_uniprot(AscNum)
+uniprotIDs <- 
+samppdbIDs <- c("1ZMR","2HWG")
+str_info <- fetch_pdb(samppdbIDs)
+threeDstruc <- fetch_alphafold_prediction(AscNum)
