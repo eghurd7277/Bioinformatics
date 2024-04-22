dd96503b61b79af8a3c57e0220d3f5e6bc807a79
eghurd7277 <eghurd7277@eagle.fgcu.edu>
2024-04-22 16:30:15 -0400

    Final Presentation Work

---
+++ Final_Presentation/FinalPresentation.R
@@ -0,0 +1,41 @@
+#loading necessary packages
+library(seqinr)
+library (Biostrings)
+library(BiocManager)
+library(msa)
+library(phangorn)
+library(ape)
+
+#reading in fasta files
+catus <- readDNAStringSet("/Users/emmagrace3/Desktop/bioinformatics/GitHub/Bioinformatics/Final_Presentation/catus.fasta", format = "fasta")
+silvestris <- readDNAStringSet("/Users/emmagrace3/Desktop/bioinformatics/GitHub/Bioinformatics/Final_Presentation/silvestris.fasta", format = "fasta")
+nigripes <- readDNAStringSet("/Users/emmagrace3/Desktop/bioinformatics/GitHub/Bioinformatics/Final_Presentation/nigripes.fasta", format = "fasta")
+margarita <- readDNAStringSet("/Users/emmagrace3/Desktop/bioinformatics/GitHub/Bioinformatics/Final_Presentation/margarita.fasta", format = "fasta")
+chaus <- readDNAStringSet("/Users/emmagrace3/Desktop/bioinformatics/GitHub/Bioinformatics/Final_Presentation/chaus.fasta", format = "fasta")
+
+#combining fasta files into one, rewriting seqence names
+sequences <- c(catus,silvestris,nigripes,margarita,chaus)
+names(sequences) <- c("catus_GU300839.1", "catus_GU300838.1", "silvestris_GU300940.1", "silvestris_GU300937.1", "nigripes_GU300900.1", "nigripes_GU300897.1", "margarita_GU300871.1", "margarita_GU300869.1", "chaus_GU300852.1", "chaus_GU300851.1")
+
+
+#creating a MSA using muscle
+alignment <- msa(sequences, method="Muscle")
+
+#converting to seqinr format
+aln2 <- msaConvert(alignment, type="seqinr::alignment")
+
+#computing distance matrix
+dmat <- dist.alignment(aln2,matrix="identity")
+distmat <- as.matrix(dmat)
+
+#writing to phydat format
+Alignment_phyDat <- msaConvert(alignment, type="phangorn::phyDat")
+write.phyDat(Alignment_phyDat, "alignment.fasta", format = "fasta")
+
+mydistmat <- as.dist(distmat)
+
+#building a neighbor joining tree
+myNJtree <- ape::nj(mydistmat)
+
+#plotting tree
+plot(myNJtree, "unrooted")
